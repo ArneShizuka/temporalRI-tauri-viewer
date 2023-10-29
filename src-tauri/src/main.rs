79c7066e294bd18ed8783dd60b37e87d67b7b3d0
Angelo Iacona <iaconaangelo.ai@gmail.com>
2023-10-29 18:15:56 +0100

    feat: add command to call TemporalRI and placeholders files to target and query

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::{Command, Stdio};
+
+#[tauri::command]
+async fn launch_temporal_ri(handle: tauri::AppHandle) -> String {
+    let temporal_ri_jar_path = handle
+        .path_resolver()
+        .resolve_resource("../java/TemporalRI.jar")
+        .expect("failed to resolve TemporalRi.jar");
+    let target_path = handle
+        .path_resolver()
+        .resolve_resource("../java/target1.txt")
+        .expect("failed to resolve target1.txt");
+    let query_path = handle
+        .path_resolver()
+        .resolve_resource("../java/query1.txt")
+        .expect("failed to resolve query1.txt");
+
+    let output = Command::new("java")
+        .arg("-jar")
+        .arg(&temporal_ri_jar_path)
+        .arg("-t")
+        .arg(&target_path)
+        .arg("-q")
+        .arg(&query_path)
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
+
+    return String::from_utf8(output.stdout).unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![launch_temporal_ri])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,80 @@
 import { invoke } from "@tauri-apps/api/tauri"
 
-import { Graph, AdjacencyList } from "./graph"
+// import { Graph, AdjacencyList } from "./graph"
 
 window.addEventListener("DOMContentLoaded", () => {
-    const adjList: AdjacencyList = {
-        "1": {
-            label: "a",
-            edges: [
-                {
-                    target: "2",
-                    timestamp: "3",
-                    label: "b",
-                },
-            ],
-        },
-        "2": {
-            label: "a",
-            edges: [
-                {
-                    target: "6",
-                    timestamp: "17",
-                    label: "b",
-                },
-            ],
-        },
-        "3": {
-            label: "a",
-            edges: [
-                {
-                    target: "6",
-                    timestamp: "14",
-                    label: "b",
-                },
-            ],
-        },
-        "4": {
-            label: "a",
-            edges: [
-                {
-                    target: "7",
-                    timestamp: "8",
-                    label: "b",
-                },
-            ],
-        },
-        "5": {
-            label: "a",
-            edges: [
-                {
-                    target: "7",
-                    timestamp: "2",
-                    label: "b",
-                },
-            ],
-        },
-        "6": {
-            label: "a",
-            edges: [],
-        },
-        "7": {
-            label: "a",
-            edges: [
-                {
-                    target: "6",
-                    timestamp: "13",
-                    label: "b",
-                },
-            ],
-        },
-    }
+    // const adjList: AdjacencyList = {
+    //     "1": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "2",
+    //                 timestamp: "3",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    //     "2": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "6",
+    //                 timestamp: "17",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    //     "3": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "6",
+    //                 timestamp: "14",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    //     "4": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "7",
+    //                 timestamp: "8",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    //     "5": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "7",
+    //                 timestamp: "2",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    //     "6": {
+    //         label: "a",
+    //         edges: [],
+    //     },
+    //     "7": {
+    //         label: "a",
+    //         edges: [
+    //             {
+    //                 target: "6",
+    //                 timestamp: "13",
+    //                 label: "b",
+    //             },
+    //         ],
+    //     },
+    // }
 
-    let graph = new Graph()
-    graph.buildGraph(adjList)
+    // let graph = new Graph()
+    // graph.buildGraph(adjList)
+
+    invoke<string>("launch_temporal_ri").then((output) => {
+        const cy = document.getElementById("cy")
+        if (cy !== null) cy.innerText = output
+    })
 })
